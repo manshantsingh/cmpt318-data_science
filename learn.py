468f437cdd5876f3c77b8be338ecf9cfcd386f02
Manshant Singh Kohli <mskohli@sfu.ca>
2017-12-04 15:42:46 -0800

    start trying a lot of other models

--- learn.py
+++ learn.py
@@ -2,8 +2,6 @@ import sys
 import pandas as pd
 import numpy as np
 from skimage import io
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
@@ -12,9 +10,15 @@ from sklearn.pipeline import make_pipeline
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import MultiLabelBinarizer
 
-
-# def to_names(all_masks):
-# 	return np.array(list(map(lambda m: ','.join([group_names[x] for x in m]), all_masks)))
+# techniques
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import ExtraTreeClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.neighbors import RadiusNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import RidgeClassifierCV
 
 # def main():
 df = pd.read_csv(sys.argv[1])
@@ -22,41 +26,32 @@ df = pd.read_csv(sys.argv[1])
 images = io.imread_collection(df.filename.values)
 images = np.array(list(map(lambda x: x.reshape(-1), images)))
 
-# weathers = df.weather_mask.apply(lambda x: np.array([int(a) for a in x.split(',')]))
 weathers = df.weather_mask.apply(lambda x: np.array(x.split(',')))
 masks = MultiLabelBinarizer().fit_transform(weathers)
 print("starting PCA")
 reduced_images = PCA(50).fit_transform(images)
 print("done PCA")
-X = df.drop(['Date/Time','Weather','filename','weather_mask'], axis=1).join(pd.DataFrame(reduced_images))
+combined = df.drop(['Date/Time','Weather','filename','weather_mask'], axis=1).join(pd.DataFrame(reduced_images))
+X = MinMaxScaler().fit_transform(combined)
 
-X_train, X_test, y_train, y_test = train_test_split(X, masks)
-
-def run(name, technique):
-	model = make_pipeline(
-		MinMaxScaler(),
-		technique
-	)
-	print("starting now")
+def run(name, model):
+	X_train, X_test, y_train, y_test = train_test_split(X, masks)
 	model.fit(X_train, y_train)
-	# model.fit(X_test, y_test)
-
-	print("done now")
-
 	print(name, "score:", model.score(X_test, y_test))
-	return model
+	# return model
 
-run('NN', MLPClassifier())
+# default values
+run('KNeighbors', KNeighborsClassifier())
+run('MLPClassifier', MLPClassifier())
+run('DecisionTreeClassifier', DecisionTreeClassifier()) # best default
+run('ExtraTreeClassifier (w/o S)', ExtraTreeClassifier())
+run('ExtraTreesClassifier (w/ S)', ExtraTreesClassifier())
+run('RadiusNeighborsClassifier', RadiusNeighborsClassifier())
+run('RandomForestClassifier', RandomForestClassifier())
+run('RidgeClassifierCV', RidgeClassifierCV())
+
+# changing default values
 run('KNeighbors=7', KNeighborsClassifier(n_neighbors=7))
-# group_names = pd.read_csv('groups.csv').name.values
-# predictions = model.predict(X_test)
-# results = pd.DataFrame({
-# 	'omask': y_test,
-# 	'oresult': to_names(y_test),
-# 	'pmask': predictions,
-# 	'presult': to_names(predictions)
-# })
-# results.to_csv('temp/results.csv')
 
 # if __name__ == '__main__':
 # 	main()
