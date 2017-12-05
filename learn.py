d1154967400379b7d64bac6581e8e7bb51b312a0
Manshant Singh Kohli <mskohli@sfu.ca>
2017-12-04 18:22:43 -0800

    moving it back in main function

--- learn.py
+++ learn.py
@@ -18,40 +18,40 @@ from sklearn.tree import ExtraTreeClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-# def main():
-df = pd.read_csv(sys.argv[1])
+def main():
+	df = pd.read_csv(sys.argv[1])
 
-images = io.imread_collection(df.filename.values)
-images = np.array(list(map(lambda x: x.reshape(-1), images)))
+	images = io.imread_collection(df.filename.values)
+	images = np.array(list(map(lambda x: x.reshape(-1), images)))
 
-print('Starting transforming data.')
+	print('Starting transforming data.')
 
-weathers = df.weather_mask.apply(lambda x: np.array(x.split(',')))
-masks = MultiLabelBinarizer().fit_transform(weathers)
-reduced_images = PCA(50).fit_transform(images)
-combined = df.drop(['Date/Time','Weather','filename','weather_mask'], axis=1).join(pd.DataFrame(reduced_images))
-X = MinMaxScaler().fit_transform(combined)
+	weathers = df.weather_mask.apply(lambda x: np.array(x.split(',')))
+	masks = MultiLabelBinarizer().fit_transform(weathers)
+	reduced_images = PCA(50).fit_transform(images)
+	combined = df.drop(['Date/Time','Weather','filename','weather_mask'], axis=1).join(pd.DataFrame(reduced_images))
+	X = MinMaxScaler().fit_transform(combined)
 
-print('Finished transforming data.\n')
+	print('Finished transforming data.\n')
 
-def run(name, model):
-	X_train, X_test, y_train, y_test = train_test_split(X, masks)
-	model.fit(X_train, y_train)
-	print(name, 'score:', model.score(X_test, y_test))
+	def run(name, model):
+		X_train, X_test, y_train, y_test = train_test_split(X, masks)
+		model.fit(X_train, y_train)
+		print(name, 'score:', model.score(X_test, y_test))
 
-# default values
-run('KNeighbors', KNeighborsClassifier())
-run('MLPClassifier', MLPClassifier())
-run('DecisionTreeClassifier', DecisionTreeClassifier())
-run('ExtraTreeClassifier (w/o S)', ExtraTreeClassifier())
-run('ExtraTreesClassifier (w/ S)', ExtraTreesClassifier())
-run('RandomForestClassifier', RandomForestClassifier())
+	# default values
+	run('KNeighbors', KNeighborsClassifier())
+	run('MLPClassifier', MLPClassifier())
+	run('DecisionTreeClassifier', DecisionTreeClassifier())
+	run('ExtraTreeClassifier (w/o S)', ExtraTreeClassifier())
+	run('ExtraTreesClassifier (w/ S)', ExtraTreesClassifier())
+	run('RandomForestClassifier', RandomForestClassifier())
 
-print('\nSome parameter tweaks that generally do better\n')
+	print('\nSome parameter tweaks that generally do better\n')
 
-# changing default values
-run('KNeighbors (n=7)', KNeighborsClassifier(n_neighbors=7))
-run('MLPClassifier (custom)', MLPClassifier(solver='lbfgs', activation='logistic', hidden_layer_sizes=(64,)))
+	# changing default values
+	run('KNeighbors (n=7)', KNeighborsClassifier(n_neighbors=7))
+	run('MLPClassifier (custom)', MLPClassifier(solver='lbfgs', activation='logistic', hidden_layer_sizes=(64,)))
 
-# if __name__ == '__main__':
-# 	main()
+if __name__ == '__main__':
+	main()
